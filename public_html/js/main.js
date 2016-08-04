ec45da30e7fa0437c35c86e619a9585feb1d634d
4M3 <aimme.br@gmail.com>
2016-08-03 19:29:47 -0500

    Managed to change the evolution line images and leave a message... The main problem, how to add the name (it keeps popping up at the side)... Considering adding 2-3 functions to "caught" and "battle" pokemon.

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -16,12 +16,17 @@ var types = [["img/bug.gif","bug"],
     ["img/psychic.gif","psychic"],
     ["img/rock.gif","rock"],
     ["img/water.gif","water"],
-    [" "," "]];
-var Bulbasaur = ["#001 Bulbasaur", "0.7 m", "6.9 Kg", "Bulbasaur can be seen napping in bright sunlight. There is a seed on its back. By soaking up the sun's rays, the seed grows progressively larger.", types[7],types[11], "Overgrow: When HP is below 1/3rd its maximum, power of Grass-type moves is increased by 50%.", "Chlorophyll: When sunny, the Pokémon’s Speed doubles. However, Speed will not double on the turn weather becomes Strong Sunlight.", "Starter Pokemon"];
-var Ivysaur = ["#002 Ivysaur", "1.0 m", "13.0 Kg", "There is a bud on this Pokémon's back. To support its weight, Ivysaur's legs and trunk grow thick and strong. If it starts spending more time lying in the sunlight, it's a sign that the bud will bloom into a large flower soon.", types[7],types[11], "Overgrow: When HP is below 1/3rd its maximum, power of Grass-type moves is increased by 50%.", "Chlorophyll: When sunny, the Pokémon’s Speed doubles. However, Speed will not double on the turn weather becomes Strong Sunlight.", "Evolve Bulbasaur"];
-var Venusaur = ["#003 Venusaur", "2.0 m", "100.0 Kg", "There is a large flower on Venusaur's back. The flower is said to take on vivid colors if it gets plenty of nutrition and sunlight. The flower's aroma soothes the emotions of people.", types[7],types[11], "Overgrow: When HP is below 1/3rd its maximum, power of Grass-type moves is increased by 50%.", "Chlorophyll: When sunny, the Pokémon’s Speed doubles. However, Speed will not double on the turn weather becomes Strong Sunlight.", "Evolve Ivysaur"];
-var Charmander = ["#004 Charmander", "0.6 m", "8.5 Kg", "The flame that burns at the tip of its tail is an indication of its emotions. The flame wavers when Charmander is enjoying itself. If the Pokémon becomes enraged, the flame burns fiercely.", types[4],types[15], "Blaze: When HP is below 1/3rd its maximum, power of Fire-type moves is increased by 50%.", "Solar Power: During sunshine, the Pokémon’s Special Attack raises to 1.5 times but HP decreases by 1/8th the maximum HP every turn.", "Starter Pokemon"];
-var mainarray = [ Bulbasaur, Ivysaur, Venusaur, Charmander ];
+    ["",""]];
+var Bulbasaur = ["#001 Bulbasaur", "0.7 m", "6.9 Kg", "Bulbasaur can be seen napping in bright sunlight. There is a seed on its back. By soaking up the sun's rays, the seed grows progressively larger.", types[7],types[11], "Overgrow: When HP is below 1/3rd its maximum, power of Grass-type moves is increased by 50%.", "Chlorophyll: When sunny, the Pokémon’s Speed doubles. However, Speed will not double on the turn weather becomes Strong Sunlight.", "Starter Pokemon","B"];
+var Ivysaur = ["#002 Ivysaur", "1.0 m", "13.0 Kg", "There is a bud on this Pokémon's back. To support its weight, Ivysaur's legs and trunk grow thick and strong. If it starts spending more time lying in the sunlight, it's a sign that the bud will bloom into a large flower soon.", types[7],types[11], "Overgrow: When HP is below 1/3rd its maximum, power of Grass-type moves is increased by 50%.", "Chlorophyll: When sunny, the Pokémon’s Speed doubles. However, Speed will not double on the turn weather becomes Strong Sunlight.", "Evolve Bulbasaur","F"];
+var Venusaur = ["#003 Venusaur", "2.0 m", "100.0 Kg", "There is a large flower on Venusaur's back. The flower is said to take on vivid colors if it gets plenty of nutrition and sunlight. The flower's aroma soothes the emotions of people.", types[7],types[11], "Overgrow: When HP is below 1/3rd its maximum, power of Grass-type moves is increased by 50%.", "Chlorophyll: When sunny, the Pokémon’s Speed doubles. However, Speed will not double on the turn weather becomes Strong Sunlight.", "Evolve Ivysaur","S"];
+var Charmander = ["#004 Charmander", "0.6 m", "8.5 Kg", "The flame that burns at the tip of its tail is an indication of its emotions. The flame wavers when Charmander is enjoying itself. If the Pokémon becomes enraged, the flame burns fiercely.", types[4],types[15], "Blaze: When HP is below 1/3rd its maximum, power of Fire-type moves is increased by 50%.", "Solar Power: During sunshine, the Pokémon’s Special Attack raises to 1.5 times but HP decreases by 1/8th the maximum HP every turn.", "Starter Pokemon","B"];
+var Charmeleon = ["#005 Charmeleon", "1.1 m", "19.0 Kg", "Charmeleon mercilessly destroys its foes using its sharp claws. If it encounters a strong foe, it turns aggressive. In this excited state, the flame at the tip of its tail flares with a bluish white color.", types[4], types[15],"Blaze: When HP is below 1/3rd its maximum, power of Fire-type moves is increased by 50%.", "Solar Power: During sunshine, the Pokémon’s Special Attack raises to 1.5 times but HP decreases by 1/8th the maximum HP every turn.", "Evolve Charmander","F"];
+var Charizard = ["#006 Charizard", "1.7 m", "90.5 Kg", "Charizard flies around the sky in search of powerful opponents. It breathes fire of such great heat that it melts anything. However, it never turns its fiery breath on any opponent weaker than itself.", types[4], types[5],"Blaze: When HP is below 1/3rd its maximum, power of Fire-type moves is increased by 50%.", "Solar Power: During sunshine, the Pokémon’s Special Attack raises to 1.5 times but HP decreases by 1/8th the maximum HP every turn.", "Evolve Charmeleon","S"];
+var Squirtle = ["#007 Squirtle", "0.5 m", "9.0 Kg", "Squirtle's shell is not merely used for protection. The shell's rounded shape and the grooves on its surface help minimize resistance in water, enabling this Pokémon to swim at high speeds.", types[14], types[15], "Torrent: When HP is below 1/3rd its maximum, power of Water-type moves is increased by 50%.", "Rain Dish: If Heavy Rain weather is in effect, recovers 1/16th max Hit Points at the end of the turn.", "Starter Pokemon","B"];
+var Wartortle = ["#008 Wartortle", "1.0 m", "22.5 Kg", "Its tail is large and covered with a rich, thick fur. The tail becomes increasingly deeper in color as Wartortle ages. The scratches on its shell are evidence of this Pokémon's toughness as a battler.", types[14], types[15], "Torrent: When HP is below 1/3rd its maximum, power of Water-type moves is increased by 50%.", "Rain Dish: If Heavy Rain weather is in effect, recovers 1/16th max Hit Points at the end of the turn.", "Evolve Squirtle","F"];
+var Blastoise = ["#009 Blastoise", "1.6 m", "85.5 Kg", "Blastoise has water spouts that protrude from its shell. The water spouts are very accurate. They can shoot bullets of water with enough accuracy to strike empty cans from a distance of over 160 feet.", types[14], types[15], "Torrent: When HP is below 1/3rd its maximum, power of Water-type moves is increased by 50%.", "Rain Dish: If Heavy Rain weather is in effect, recovers 1/16th max Hit Points at the end of the turn.", "Evolve Wartortle","S"];
+var mainarray = [ Bulbasaur, Ivysaur, Venusaur, Charmander, Charmeleon, Charizard, Squirtle, Wartortle, Blastoise ];
 
 function plus(){
     if (num<152){
@@ -64,7 +69,7 @@ function minus (){
     location();
     baseimg ();
     firstimg ();
-    secondimg ()
+    secondimg ();
     update = "off";
     console.log(update);
  }
@@ -163,31 +168,41 @@ function type1() {
     }
 
 function type2 (){
-if (mainarray[num][5][0] !== ""){
   if (update === "off"){  
-     var img = document.createElement("img");
+    if (mainarray[num][5][0] !== ""){
+      var img = document.createElement("img");
       img.src = mainarray[num][5][0];
       img.alt = mainarray[num][5][1];
       img.style.margin = "0 3px";
-     document.getElementById("type").appendChild(img);  
+     document.getElementById("type").appendChild(img);
+     console.log(document.getElementById("type").childNodes);
+    }
+    else{
+      var t = document.createTextNode(mainarray[num][5][0]);
+    var img = document.getElementById("type");     
+     img.appendChild(t);  
+    }
+   }
+  else if (update === "on") {
+    if (mainarray[num][5][0] === ""){
+     var t = document.createTextNode(mainarray[num][5][0]);
+    var img = document.getElementById("type");     
+     img.replaceChild(t,img.childNodes[3]);
+     console.log(document.getElementById("type").childNodes);
     }
 
     else{
-      uimg = document.createElement("img");
+      var uimg = document.createElement("img");
       uimg.src = mainarray[num][5][0];
       uimg.alt = mainarray[num][5][1];
       uimg.style.margin = "0 3px";
-     img = document.getElementById("type");     
-     img.replaceChild(uimg, img.childNodes[3]);   
+       img = document.getElementById("type");     
+     img.replaceChild(uimg, img.childNodes[3]);  
+     console.log(document.getElementById("type").childNodes);
+     
     }
-    }
-  else {
-     rem = document.getElementById("type");
-             rem.removeChild(rem.childNodes[3]);
-             console.log(rem.childNodes);
-      
   }
-    }
+ }
 
 function ability (){
     if (update === "off"){
@@ -229,21 +244,57 @@ function location (){
 }
 function baseimg (){
     if (update === "off"){
-        if (num === 0 || num === 3 || num === 6){
+        if (mainarray[num][9]==="B"){
           var img = document.createElement("img");
       img.src = "img/"+ num +".png";
       img.alt = mainarray[num][0];
       img.style.width = "50%";
       img.style.height = "50%";
+       var t = document.createTextNode(mainarray[num][0]);
+       console.log(t);
+     document.getElementById("baseimg").appendChild(img);
+      document.getElementById("baseimg").appendChild(t);
+        }
+        else if (mainarray[num][9]==="F"){
+          var img = document.createElement("img");
+      img.src = "img/"+ (num-1) +".png";
+      img.alt = mainarray[(num-1)][0];
+      img.style.width = "50%";
+      img.style.height = "50%";
      document.getElementById("baseimg").appendChild(img); 
-          console.log(document.getElementById("baseimg").childNodes);
+        }
+        else if (mainarray[num][9]==="S"){
+          var img = document.createElement("img");
+      img.src = "img/"+ (num-2) +".png";
+      img.alt = mainarray[(num-2)][0];
+      img.style.width = "50%";
+      img.style.height = "50%";
+     document.getElementById("baseimg").appendChild(img); 
         }
     }
     else{
-      if (num === 0 || num === 3 || num === 6){
+      if (mainarray[num][9]==="B"){
           uimg = document.createElement("img");
       uimg.src = "img/"+ num +".png";
       uimg.alt = mainarray[num][0];
+      uimg.style.width = "50%";
+      uimg.style.height = "50%";
+     img = document.getElementById("baseimg");     
+     img.replaceChild(uimg, img.childNodes[2]); 
+        }
+      else if (mainarray[num][9]==="F"){
+          uimg = document.createElement("img");
+      uimg.src = "img/"+ (num-1) +".png";
+      uimg.alt = mainarray[(num-1)][0];
+      uimg.style.width = "50%";
+      uimg.style.height = "50%";
+     img = document.getElementById("baseimg");     
+     img.replaceChild(uimg, img.childNodes[2]); 
+        }
+      else if (mainarray[num][9]==="S"){
+          uimg = document.createElement("img");
+      uimg.src = "img/"+ (num-2) +".png";
+      uimg.alt = mainarray[(num-2)][0];
       uimg.style.width = "50%";
       uimg.style.height = "50%";
      img = document.getElementById("baseimg");     
@@ -253,50 +304,138 @@ function baseimg (){
 }
 
 function firstimg (){
-    if (update === "off"){
-        if (num === 0 || num === 3 || num === 6){
+   if (update === "off"){
+        if (mainarray[num][9]==="B" && mainarray[(num + 1)][9]==="F"){
           var img = document.createElement("img");
-      img.src = "img/"+ (num + 1) +".png";
-      img.alt = mainarray[(num + 1)][0];
+      img.src = "img/"+ (num+1) +".png";
+      img.alt = mainarray[(num+1)][0];
       img.style.width = "50%";
       img.style.height = "50%";
-     document.getElementById("firstimg").appendChild(img);
+     document.getElementById("firstimg").appendChild(img); 
+        }
+        else if (mainarray[num][9]==="F"){
+          var img = document.createElement("img");
+      img.src = "img/"+ num +".png";
+      img.alt = mainarray[num][0];
+      img.style.width = "50%";
+      img.style.height = "50%";
+     document.getElementById("firstimg").appendChild(img); 
+        }
+        else if (mainarray[num][9]==="S"){
+          var img = document.createElement("img");
+      img.src = "img/"+ (num-1) +".png";
+      img.alt = mainarray[(num-1)][0];
+      img.style.width = "50%";
+      img.style.height = "50%";
+     document.getElementById("firstimg").appendChild(img); 
+        }
+        else {
+        var x = document.getElementById("firstimg"); 
+     var t = document.createTextNode("Pokemon does not have further evolution.");
+      x.appendChild(t); 
         }
     }
-
     else{
-      if (num === 0 || num === 3 || num === 6){
+      if (mainarray[num][9]==="B" && mainarray[(num + 1)][9]==="F"){
           uimg = document.createElement("img");
-      uimg.src = "img/"+ (num + 1) +".png";
-      uimg.alt =  mainarray[(num + 1)][0];
+      uimg.src = "img/"+ (num+1) +".png";
+      uimg.alt = mainarray[(num+1)][0];
       uimg.style.width = "50%";
       uimg.style.height = "50%";
      img = document.getElementById("firstimg");     
      img.replaceChild(uimg, img.childNodes[2]); 
         }
+      else if (mainarray[num][9]==="F"){
+          uimg = document.createElement("img");
+      uimg.src = "img/"+ num +".png";
+      uimg.alt = mainarray[num][0];
+      uimg.style.width = "50%";
+      uimg.style.height = "50%";
+     img = document.getElementById("firstimg");     
+     img.replaceChild(uimg, img.childNodes[2]); 
+        }
+      else if (mainarray[num][9]==="S"){
+          uimg = document.createElement("img");
+      uimg.src = "img/"+ (num-1) +".png";
+      uimg.alt = mainarray[(num-1)][0];
+      uimg.style.width = "50%";
+      uimg.style.height = "50%";
+     img = document.getElementById("firstimg");     
+     img.replaceChild(uimg, img.childNodes[2]); 
+        }
+     else {
+        var x = document.getElementById("firstimg"); 
+     var t = document.createTextNode("Pokemon does not have further evolution.");
+      x.replaceChild(t, x.childNodes[2]);
+        }
     }
+    
 }
 
 function secondimg (){
     if (update === "off"){
-if (num === 0 || num === 3 || num === 6){
+        if (mainarray[num][9]==="B"&& mainarray[(num+1)][9]==="F" && mainarray[(num+2)][9]==="S"){
           var img = document.createElement("img");
-      img.src = "img/"+ (num + 2) +".png";
-      img.alt = mainarray[(num + 2)][0];
+      img.src = "img/"+ (num+2) +".png";
+      img.alt = mainarray[(num+2)][0];
       img.style.width = "50%";
       img.style.height = "50%";
-     document.getElementById("secondimg").appendChild(img);   
+     document.getElementById("secondimg").appendChild(img); 
+        }
+        else if (mainarray[num][9]==="F" && mainarray[(num+1)][9]==="S"){
+          var img = document.createElement("img");
+      img.src = "img/"+ (num+1) +".png";
+      img.alt = mainarray[(num+1)][0];
+      img.style.width = "50%";
+      img.style.height = "50%";
+     document.getElementById("secondimg").appendChild(img); 
+        }
+        else if (mainarray[num][9]==="S"){
+          var img = document.createElement("img");
+      img.src = "img/"+ (num) +".png";
+      img.alt = mainarray[(num)][0];
+      img.style.width = "50%";
+      img.style.height = "50%";
+     document.getElementById("secondimg").appendChild(img); 
+        }
+        else {
+        var x = document.getElementById("secondimg"); 
+     var t = document.createTextNode("Pokemon does not have further evolution.");
+      x.appendChild(t); 
         }
     }
     else{
-        if (num === 0 || num === 3 || num === 6){
+      if (mainarray[num][9]==="B"&& mainarray[(num+1)][9]==="F"&& mainarray[(num+2)][9]==="S"){
           uimg = document.createElement("img");
-      uimg.src = "img/"+ (num + 2) +".png";
-      uimg.alt = mainarray[(num + 2)][0];
+      uimg.src = "img/"+ (num+2) +".png";
+      uimg.alt = mainarray[(num+2)][0];
       uimg.style.width = "50%";
       uimg.style.height = "50%";
      img = document.getElementById("secondimg");     
      img.replaceChild(uimg, img.childNodes[2]); 
+        }
+      else if (mainarray[num][9]==="F"&& mainarray[(num+1)][9]==="S"){
+          uimg = document.createElement("img");
+      uimg.src = "img/"+ (num+1) +".png";
+      uimg.alt = mainarray[(num+1)][0];
+      uimg.style.width = "50%";
+      uimg.style.height = "50%";
+     img = document.getElementById("secondimg");     
+     img.replaceChild(uimg, img.childNodes[2]); 
+        }
+      else if (mainarray[num][9]==="S"){
+          uimg = document.createElement("img");
+      uimg.src = "img/"+ (num) +".png";
+      uimg.alt = mainarray[(num)][0];
+      uimg.style.width = "50%";
+      uimg.style.height = "50%";
+     img = document.getElementById("secondimg");     
+     img.replaceChild(uimg, img.childNodes[2]); 
+        }
+       else {
+        var x = document.getElementById("secondimg"); 
+     var t = document.createTextNode("Pokemon does not have further evolution.");
+      x.replaceChild(t, x.childNodes[2]);
         }
     }
 }
